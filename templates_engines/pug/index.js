cf764fa8b3022ed6bd12b421744fa30da4076f3f
ad <adrian.fernandez@idom.com>
2019-12-03 22:12:32 -0600

    More dependencies injections, more modularity, less coupling

---
+++ templates_engines/pug/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+let ex = require('./controllers/express')(app);
+let personas = require('./models/persona').personas;
+
+ex.set('view engine', 'pug');
+ex.listen(3000, 'Aplicación corriendo en el puerto 3000!');
+
+let initial = {
+    titulo : 'Pug',
+    mensaje : 'Prueba',
+    personas : personas 
+};
+
+require('./controllers/routes')(app, initial);
